bcce7ef901e29e36023e9592983adaaa19e6f7af
mwafy092 <mwafy092@gmail.com>
2020-07-12 02:24:22 +0200

    JS update 5

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const navList = document.querySelector('#navbar-list');
 const sections = document.querySelectorAll('section');
 const toggle = document.querySelector('#nav-toggle');
+
 /*
     navbar toggler on mobile
 */
@@ -31,7 +32,7 @@ const navBuilder = () => {
         let navName = section.dataset.nav;  // fetch the data from each section 
 
         // create the navigation items
-        navItems += `<li><a href="#${section.id}">${navName}</a></li>`
+        navItems += `<li><a href="#" data-link="${navName}">${navName}</a></li>`
     })
 
     // append data as HTML to navList ul container
@@ -43,18 +44,57 @@ navBuilder();   // calling navbar builder
 
 
 /* 
-    distinguish the section in view
+    distinguish the section and nav item in view
  */
+
+const navItems = document.querySelectorAll('li a');    // group all nav Items
 
 function scrollActive() {
     sections.forEach(section => {
         let cord = Math.floor(section.getBoundingClientRect().top);
-        if (cord >= -150 && cord < 150) {
-            section.classList.add('active')
+        if (cord >= -300 && cord < 300) {
+            section.classList.add('active');
+
+            // loop through all nav items
+            navItems.forEach(navItem => {
+                // choose which link is active based on active section
+                if (navItem.dataset.link === section.dataset.nav) {
+                    navItem.classList.add('active-link')
+                } else {
+                    navItem.classList.remove('active-link')
+                }
+            })
+
         } else {
             section.classList.remove('active')
         }
     })
 }
+
 window.addEventListener('scroll', scrollActive);
+
+
+/* 
+    scroll into view 
+*/
+function scrolling() {
+    // loop through nav bar items
+    navItems.forEach(navItem => {
+        navItem.addEventListener('click', (e) => {
+            e.preventDefault(); // prevent default click action
+
+            // loop through each section
+            sections.forEach(section => {
+
+                // choose the right section to scroll to
+                if (section.dataset.nav === navItem.dataset.link) {
+                    // use the top position related to the selected section
+                    section.scrollIntoView({ behavior: "smooth", top: section.getBoundingClientRect().top });
+                }
+            })
+        })
+    })
+}
+
+scrolling();    // invoking scrolling function
 
